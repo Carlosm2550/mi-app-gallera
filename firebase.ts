00afd37620b473f1598dde3b1b474649cfa58eda
Carlosm2550 <carlostecontacta@gmail.com>
2025-07-25 08:36:25 -0500

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -19,4 +19,4 @@ const app = initializeApp(firebaseConfig);
 // Export the services we will use throughout the app
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export { firebaseConfig, initializeApp }; // Export config for temporary app instances
+export { firebaseConfig }; // Export config for temporary app instances
